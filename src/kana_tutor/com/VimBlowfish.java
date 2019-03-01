103098d936134bf52021617317d3bfe56bc1bd28
sjs <sjs@sonic.net>
2019-02-28 19:15:43 -0800

    initial ckin.  some debug stuff is working.

---
+++ src/kana_tutor/com/VimBlowfish.java
@@ -0,0 +1,104 @@
+package kana_tutor.com;
+
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+
+public class VimBlowfish {
+
+    private static boolean isPrintable(byte b) {
+        return b >= ' ' && b <= '~';
+    }
+
+    private static String bytesToString(byte[] bytesIn) {
+        String rv = "";
+        StringBuffer sb = null;
+
+        for(int i = 0; i < bytesIn.length; i += 16) {
+            sb = new StringBuffer(String.format("%3d)", i));
+            for (int j = 0; j < 16 && i + j < bytesIn.length; j++) {
+                sb.append(String.format(" %02x", bytesIn[i + j] & 0xff));
+                if (j == 7 && bytesIn.length > 7) sb.append(" |");
+            }
+            while (sb.length() < 58) sb.append(" ");
+            for (int j = 0; j < 16; j++) {
+                byte b = bytesIn[i + j];
+                sb.append(String.format("%c", (isPrintable(b)) ? (char) b : 'ø'));
+                if (j == 7) sb.append(" | ");
+            }
+            sb.append("\n");
+            rv += sb.toString();
+        }
+        return rv;
+    }
+
+    /*
+        from Python vimBlowfish
+        import hashlib
+        def pwToKey(pw, salt):
+          for i in range(1000):
+            pw = hashlib.sha256(pw.encode() + salt).hexdigest()
+        return hashlib.sha256(pw.encode() + salt).digest()
+
+        For salt = 09 5b 17 da dc d0 b7, pw = 'hello',
+        result should be 32 bytes:
+          f0 28 69 c9 2c 50 c3 | c2 d7 2c 4e 41 12 57 2b
+          64 5e 44 f1 36 51 30 | 6e 68 3c 95 d3 84 41 f1
+
+        First pass, bytes for 'hello' + seed is:
+          68 65 6c 6c 6f 09 5b | da dc d0 b7 16
+        hashlib.sha256(x).hexdigest() gives 64 byte string:
+        'd7dc49413209238b057a8613deff90a0c1d8b0ffb34e484e7d85b31d0d7fce15'
+
+    */
+    private static String hexdigest(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(bytesToString(bytes));
+        for (byte b : bytes)
+            sb.append(String.format("%02x", b & 0xff));
+        return sb.toString();
+    }
+    private static byte[] passwordToKey(String pw, byte[] salt)
+            throws NoSuchAlgorithmException {
+        byte[] rv = {};
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        ByteBuffer inputBytes = ByteBuffer.allocate(512);
+        inputBytes.put(pw.getBytes());
+        inputBytes.put(salt);
+        String hexString = null;
+        for(int i = 0; i < 1000; i++) {
+            md.update(inputBytes);
+            hexString = hexdigest(md.digest());
+        }
+
+        return rv;
+    }
+    private static byte[] xor(byte[] a, byte[] b) {
+        int min = (a.length < b.length) ? a.length : b.length;
+        byte[] c = new byte[min];
+        for (int i = 0; i < min; i++)
+            c[i] = (byte) ((a[i] ^ b[i]) & 0xff);
+        return c;
+    }
+
+    VimBlowfish() {
+        byte[] seed = {
+            (byte)0x09, (byte)0x5b, (byte)0x17, (byte)0xda
+            , (byte)0xdc, (byte)0xd0, (byte)0xb7, (byte)0x16
+        };
+        String pw = "hello";
+
+        try {
+            byte[] key = passwordToKey(pw, seed);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
+
